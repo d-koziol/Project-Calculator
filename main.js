e9051ed4659853739d61c7d8c2d33e520c883006
Damian Kozioł <d.p.koziol@gmail.com>
2022-12-28 00:52:36 +0100

    Function "operate" added.

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 function add(a,b){
-    const sum = a + b;
-    return sum;
+    return a + b;
 }
 console.log(add(2,3))
 
 
 function substract(a,b){
-    const sum = a - b;
-    return sum;
+    return a - b;
 }
 console.log(substract(2,3))
 
 
 function multiply(a,b){
-    const sum = a * b;
-    return sum;
+    return a * b;
 }
 console.log(multiply(2,3))
 
 
 function divide(a,b){
-    const sum = a / b;
-    return sum;
+    return a / b;
 }
 console.log(divide(6,3))
+
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return substract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+};
+
+console.log(operate(1,2,"+"));
