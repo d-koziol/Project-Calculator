8dbcf8bc61f9406a0ca96d197e7462b29c9a04ce
Damian Kozioł <d.p.koziol@gmail.com>
2022-12-28 00:28:19 +0100

    HTML, CSS and JS files created. Functions add, substract, multiply and divide coded.

---
+++ main.js
@@ -0,0 +1,26 @@
+function add(a,b){
+    const sum = a + b;
+    return sum;
+}
+console.log(add(2,3))
+
+
+function substract(a,b){
+    const sum = a - b;
+    return sum;
+}
+console.log(substract(2,3))
+
+
+function multiply(a,b){
+    const sum = a * b;
+    return sum;
+}
+console.log(multiply(2,3))
+
+
+function divide(a,b){
+    const sum = a / b;
+    return sum;
+}
+console.log(divide(6,3))
